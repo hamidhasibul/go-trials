ea3df282e12ef4f1aef00d8e0d4f315eb34b9124
Hasibul Hamid <hamidhasibul@gmail.com>
2024-02-26 15:27:54 +0600

    test: added two new tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,19 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, got %v", len(d))
 	}
+
+	if d[0] != "1 of Spades" {
+		t.Errorf("Expected first card of 1 of Spades, but got %v", d[0])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected 52 cards got %v", len(loadedDeck))
+	}
+	os.Remove("_decktesting")
 }
